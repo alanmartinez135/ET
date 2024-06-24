6c39163afc65642c05b3c4ce24d909ad04fc1b48
Javiera <ja.acuna@duocuc.cl>
2024-06-23 21:45:29 -0400

    .

--- LaTiendita/urls.py
+++ LaTiendita/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('identificador/', views.identificador, name = 'identificador'),
     path('descrip-carrito/', views.descrip_carro, name='descrip-carrito'),
     path('agregar/<int:producto_id>/', views.agregar_producto, name='add'),
+    path('agregar1/<int:producto_id>/', views.agregar_producto1, name='add1'),
     path('eliminar/<int:producto_id>/', views.eliminar_producto, name='del'),
     path('restar/<int:producto_id>/', views.restar_producto, name='sub'),
     path('limpiar/', views.limpiar_carrito, name='cls'),
